9944fe433a3b11e0a9b3759416e31fb61e0e2d09
lunathiago <lunathiago14@gmail.com>
2025-04-14 22:52:11 -0300

    Update error_handler.ts

--- error/error_handler.ts
+++ error/error_handler.ts
@@ -1,6 +1,6 @@
 import{type Response,type Request,type NextFunction} from 'express';
 
-export const error_handler = (err:Error,req:Request,res:Response,next:NextFunction)=>{
+export default function error_handler = (err:Error,req:Request,res:Response,next:NextFunction)=>{
     console.error(err.stack);
     res.status(500).json({message:err.message});
 };
